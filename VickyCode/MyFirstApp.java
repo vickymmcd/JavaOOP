7046132280d1d6bca9cb526b5011c6cffd264745
vickymmcd <vickymmcd@gmail.com>
2016-11-22 21:42:22 -0500

    first commit

---
+++ VickyCode/MyFirstApp.java
@@ -0,0 +1,13 @@
+public class MyFirstApp{
+	public static void main(String[] args){
+		System.out.println("I rule the world!");
+		int x=5;
+		while (x>0){
+			System.out.println(x + "***");
+			x = x-1;
+		}
+		if (x==0){
+			System.out.println("I am 0...");
+		}
+	}
+}
